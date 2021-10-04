1ce1f6d246667378bddd683ed3aa167f88b36137
PanchenkoAleksandr <aleksandr.panchenko@tut.by>
2021-10-04 23:04:08 +0300

    feat: done some task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,25 +4,59 @@ import { NotImplementedError } from '../extensions/index.js';
  * Implement chainMaker object according to task description
  * 
  */
+// export default {
+//   getLength() {
+//     throw new NotImplementedError('Not implemented');
+//     // remove line with error and write your code here
+//   },
+//   addLink(/* value */) {
+//     throw new NotImplementedError('Not implemented');
+//     // remove line with error and write your code here
+//   },
+//   removeLink(/* position */) {
+//     throw new NotImplementedError('Not implemented');
+//     // remove line with error and write your code here
+//   },
+//   reverseChain() {
+//     throw new NotImplementedError('Not implemented');
+//     // remove line with error and write your code here
+//   },
+//   finishChain() {
+//     throw new NotImplementedError('Not implemented');
+//     // remove line with error and write your code here
+//   }
+// };
+
+
+
+// let chainMaker = {
 export default {
+  links: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.links.length
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    if (typeof value === 'undefined') {
+      value = ' ( ) '
+    }
+    this.links.push(`( ${value} )`)
+    return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (typeof position === 'number' || Number.isInteger(position) || typeof this.links[position - 1] !== 'undefined') {
+      this.links.splice(position - 1, 1)
+      return this
+    } else {
+      throw new Error("You can't remove incorrect link!")
+    }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.links.reverse()
+    return this
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const result = this.links.join('~~')
+    this.links = []
+    return result
   }
-};
+}
